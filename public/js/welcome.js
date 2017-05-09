5d28e646944804ab4821b88db9107aff3d7a8e14
Richard Truong <reket1990@yahoo.com>
2017-05-09 01:37:51 -0700

    add riffle deck

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -2,8 +2,11 @@
  * JavaScript controlling the welcome page
  */
 
+
+/*
+ * Makes start button load the trick
+ */
 $('#start-button').click(function() {
-    // Load magic trick
     $('#home-cell').fadeOut(
         800,
         function() {
@@ -12,6 +15,10 @@ $('#start-button').click(function() {
     );
 });
 
+
+/*
+ * Main function that plays the trick
+ */
 function playTrick() {
     // Display instructions
     $("#instructions")
@@ -22,9 +29,57 @@ function playTrick() {
         .fadeOut(function() { $(this).text("Try to see one card in the deck") })
         .fadeIn()
         .delay(2000)
-        .fadeOut(function() { $(this).text("Not the bottom card...try to focus on one inside") })
+        .fadeOut(function() { $(this).text("Not the bottom card... try to focus on one inside") })
         .fadeIn();
 
+    // Raises and riffles the deck
+    setTimeout((function() {
+        riffleDeck();
+    }), 8000);
+
+    // Add functions to buttons
+    $('#trick-button-1').click(function() {
+        console.log("trick complete");
+    });
+    $('#trick-button-2').click(function() {
+        // Hide buttons
+        $('#trick-buttons').hide();
+
+        setTimeout((function() {
+            riffleDeck();
+        }), 500);
+
+        $('#trick-buttons').delay(6000).fadeTo(1000, 1);
+    });
+
     // Reveal buttons
-    $('#trick-buttons').delay(10000).fadeTo(1000, 1);
+    $('#trick-buttons').delay(14000).fadeTo(1000, 1);
+}
+
+
+/*
+ * Raises and riffles the deck. Then resets the deck to be riffle-able again.
+ *
+ * Total animation:  5000 ms
+ *
+ * Raise deck:       1300 ms
+ * Pause:            2700 ms
+ * Riffle:           1000 ms
+ */
+function riffleDeck() {
+    // Raise deck
+    $("#deck-top").animate({ borderTopLeftRadius: 10, borderTopRightRadius: 10 }, 1300);
+    $("#card").animate({ height: 100 }, 1300);
+
+    // Riffle deck
+    setTimeout((function() {
+        $("#deck-bottom").animate({ height: 75 }, 1000);
+        $("#deck-top").animate({ borderTopLeftRadius: 0, borderTopRightRadius: 0, height: 0 }, 1000, function() {
+            $("#card").css("height", "0");
+        });
+    }), 4000);
+
+    // Reset deck
+    $("#deck-top").css("height", "75px");
+    $("#deck-bottom").css("height", "0");
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,26 +47,41 @@
                 font-weight: bold;
             }
 
+            /* Instructions two lines on mobile */
+            #instructions {
+                min-height: 44px;
+            }
+            @media only screen and (min-width : 480px) {
+                #instructions{
+                    min-height: 0;
+                }
+            }
+
             /* Deck */
-            #deck {
-                margin-bottom: 22px;  /* matches .lead */
+            .deck-wrapper {
+                position: relative;
+                height: 175px;
+                margin-bottom: 27px;
+            }
+            .deck {
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                right: 0;
             }
             #deck-top {
                 height: 75px;
-                border-radius: 10px 10px 0 0;
             }
             #card {
-                height: 100px;
+                height: 0;
                 overflow: hidden;
+                min-width: 222px;
             }
             #card img {
                 background-color: white;
                 border-radius: 10px 10px 0 0;
                 transform: rotateX(50deg);
                 transform-origin: top;
-            }
-            #deck-bottom {
-                height: 75px;
             }
             .stripes {
                 width: 222px;
@@ -97,14 +112,16 @@
 
             <div class="page-wrapper-cell hidden" id="trick-cell">
                 <p class="lead" id="instructions">Here is a deck of cards</p>
-                <div id="deck">
-                    <div class="stripes" id="deck-top"></div>
-                    <div id="card"><img src="{{ asset('images/cards/png/red_joker.png') }}"></div>
-                    <div class="stripes" id="deck-bottom"></div>
+                <div class="deck-wrapper">
+                    <div class="deck">
+                        <div class="stripes" id="deck-top"></div>
+                        <div id="card"><img src="{{ asset('images/cards/png/red_joker.png') }}"></div>
+                        <div class="stripes" id="deck-bottom"></div>
+                    </div>
                 </div>
-                <p class="lead" id="trick-buttons opaque">
-                    <a href="#" class="btn btn-lg" id="button-1">Got One</a>
-                    <a href="#" class="btn btn-lg" id="button-2">Again</a>
+                <p class="lead opaque" id="trick-buttons">
+                    <a href="#" class="btn btn-lg" id="trick-button-1">Got One</a>
+                    <a href="#" class="btn btn-lg" id="trick-button-2">Again</a>
                 </p>
             </div>
         </div>
